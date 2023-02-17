9df6b565165444e762cd4a3a8590d379dfa079c2
Helly Shah <hellyshah@google.com>
2023-02-17 10:08:00 -0500

    Add all files for challenge

---
+++ seamantics.js
@@ -0,0 +1,19 @@
+function calculate() {
+    var num_octopus = document.getElementById("octopus").value
+
+    // !!!!!!!!!!!!!!!!!! STEP 5 !!!!!!!!!!!!!!!!!!
+    // Get the no. of seahorses from the user input and store it in a variable
+    // Get the no. of starfish from the user input and store it in a variable
+    // Get the no. of other beings from the user input and store it in a variable
+
+    // !!!!!!!!!!!!!!!!!! STEP 6 !!!!!!!!!!!!!!!!!!
+    // Calculate the total no. of water-blades needed and store it in a variable
+
+    // !!!!!!!!!!!!!!!!!! STEP 7 !!!!!!!!!!!!!!!!!!
+    // Embed the result in the modal text body.
+}
+
+// !!!!!!!!!!!!!!!!!! STEP 8 !!!!!!!!!!!!!!!!!!
+// Call the function defined above when the user clicks the button to get the total no. of water-blades needed
+
+
